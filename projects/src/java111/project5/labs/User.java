0c5e3a8250968dd6e46cc6636f1b03fe6ba29465
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2024-12-07 11:39:51 -0600

    Cleaned up remaining javadocs

---
+++ projects/src/java111/project5/labs/User.java
@@ -0,0 +1,16 @@
+package java111.project5.labs;
+
+/**
+ *  This super class holds information about a User (for Unit 5 Lab 2)
+ *  @author Nick Hanson
+ *      - Created 12/6/24
+ */
+public class User {
+    
+    /**
+    *  No params constructor for the User object.
+    */
+    public User() {
+        System.out.println("I am the User Class");
+    }
+}
